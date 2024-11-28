9d31c441770d3e783be86264f2029b5a6504fb1d
ZORO333 <53059662+ZORO333@users.noreply.github.com>
2024-11-28 11:34:00 +0100

    Book

---
+++ Models/Book.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace library.Models
+{
+    public class Book
+    {
+        public int ID {get; set;}
+        public string Title {get; set;}
+
+        public DateTime published {get; set;}
+        public ICollection<Author> Authors{get; set;}
+    }
+}
