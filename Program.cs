89832fedd50b2c45bc0efa5017b314eb8f553bb8
ZORO333 <53059662+ZORO333@users.noreply.github.com>
2024-11-29 10:24:49 +0100

    Skapat en liten meny

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection.Metadata;
+using System.Runtime.ConstrainedExecution;
+using library.Models;
+
+public class mylibrary
+{
+    public static void Main()
+    {
+        System.Console.WriteLine("Min biblotek");
+        System.Console.WriteLine("1. Lägg till böcker");
+        System.Console.WriteLine("2. Stäng av bibloteket");
+
+        bool run = true;
+
+        while (run)
+        {
+            string val = Console.ReadLine();
+            switch (val)
+            {
+                case "1":
+
+                    Add.Addbook();
+                    System.Console.WriteLine("Böckerna har lagts till");
+                    break;
+                case "2":
+                    run = false;
+                    System.Console.WriteLine("Biblotekt har stängts");
+                    break;
+                default:
+                    System.Console.WriteLine("Fel. Försök agin...");
+                    break;
+
+            }
+        }
+    }
+}
